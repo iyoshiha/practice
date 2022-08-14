04c38920b48d7a0f603c57df2d454af1a924f8f9
iyoshiha <yoshi3co@gmail.com>
2022-08-14 16:30:46 +0900

    data type

--- 20220814-go/src/hello.go
+++ 20220814-go/src/hello.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	var x int = 10
+	var y float64 = 30.2
+	var z float64 = float64(x) + y
+	var d int = x + int(y)
 
-	fmt.
-		Println("Hello, world! Go introcuction")
+	fmt.Println(z)
+	fmt.Println(d)
 }
