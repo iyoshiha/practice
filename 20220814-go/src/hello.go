93a42d1e2bcdd48b80e2cb2dff2243b4f11ce5d3
iyoshiha <yoshi3co@gmail.com>
2022-08-27 21:38:25 +0900

    a

--- 20220814-go/src/hello.go
+++ 20220814-go/src/hello.go
@@ -5,6 +5,7 @@ import "fmt"
 var global = 20
 
 func main() { 
+	m := 80
 	const x int = 10
 	const (
 		idKey = "id"
@@ -13,6 +14,7 @@ func main() {
 	const z = 20 * 10
 
 	fmt.Println(x)
+	fmt.Println(m)
 	fmt.Println(idKey)
 	fmt.Println(namekey)
 	fmt.Println(z)
