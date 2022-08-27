220b807e0c094f8a1b4f1b7be825c01532fc3bab
iyoshiha <yoshi3co@gmail.com>
2022-08-27 20:32:45 +0900

    short declaration in function

--- 20220814-go/src/hello.go
+++ 20220814-go/src/hello.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func main() {
-	var x int = 10
-	var y float64 = 30.2
-	var z float64 = float64(x) + y
-	var d int = x + int(y)
+var global = 20
 
-	fmt.Println(z)
-	fmt.Println(d)
+func main() { 
+	x := 10
+	x, y := 20, "hello"
+
+	fmt.Println(y)
+	fmt.Println(x)
 }
