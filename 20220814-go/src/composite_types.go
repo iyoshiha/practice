0d1fcd990070409b64aea6e32fbcfd329262fad6
iyoshiha <yoshi3co@gmail.com>
2022-08-28 23:13:37 +0900

    nil map and empty value map

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -4,27 +4,15 @@ import "fmt"
 
 func main(){
 
-	var x string = "hello world!"
-	var b byte = x[6]
-	var y = []int{1,2,5:10,3,4,18:100}
+	// map with string key and int values
+	var nilMap map[string]int // nil map
+	var totalWins = map[string]int{} // empty map
 
-	fmt.Println("===== x =====")
-	fmt.Println(string(b))
-	fmt.Println("===== y =====")
-	fmt.Println(y)
+	fmt.Println("===== nilMap =====")
+	fmt.Println(nilMap)
+	fmt.Println("===== totalWins =====")
+	fmt.Println(totalWins)
 	fmt.Println("===== make =====")
 	
-	var l rune = 'あ'
-	fmt.Println(l)
-	var n int = 65
-	var nn = string(n) // int to string is not hobidden but not recommended
-	fmt.Println(nn)
-
-	var nnn string = "hello, ☺ A"
-	var bs []byte = []byte(nnn)
-	var rs []rune= []rune(nnn)
-	fmt.Println(nnn)
-	fmt.Println(bs)
-	fmt.Println(rs)
 
 }
