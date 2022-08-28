279c86820a6b1fc8ca91323e153212a21939473e
iyoshiha <yoshi3co@gmail.com>
2022-08-28 13:00:25 +0900

    slice : capacity

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -13,13 +13,15 @@ func main(){
 	var x = [21]int{1,5:4,6,1:100,15}
 
 	var s []int;
-	var ss = []int{2,3,4,5,6}
+	var ss = []int{2,3,4,5}
 	// ture
 	fmt.Println(y == z);
 	fmt.Println(len(k[0]));
 	fmt.Println(x)
 	s = append(s,4)
 	s = append(s,ss...)
-	fmt.Println(s)
+	fmt.Println(cap(s))
+	s = append(s,4)
+	fmt.Println(cap(s))
 	
 }
