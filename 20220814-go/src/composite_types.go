6a1158e3a3b9ca2273b3887f52bb0d5a9725d735
iyoshiha <yoshi3co@gmail.com>
2022-08-29 01:08:07 +0900

    set

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -53,5 +53,21 @@ func main(){
 	delete(m, "hello")
 	fmt.Println("delete function\n", m)	
 
+	// set 
+	fmt.Println("======== set ======== ") 
+
+	intSet := map[int]bool{}
+	vals := []int{5, 10, 2, 5, 8, 7, 3, 9, 1, 2, 10}
+	for _, v := range vals{
+		fmt.Println(intSet[v])
+		intSet[v] = true
+		fmt.Println(v)
+	}
+	fmt.Println(len(vals), len(intSet))
+	fmt.Println(intSet[5])
+	fmt.Println(intSet[500])
+	if intSet[100] {
+		fmt.Println("100 is in the set")
+	}
 
 }
