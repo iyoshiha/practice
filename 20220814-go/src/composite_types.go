7778832071e48a9cf06f79a59c185792976b6661
iyoshiha <yoshi3co@gmail.com>
2022-08-28 22:23:43 +0900

    strings bytes runes

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -4,34 +4,21 @@ import "fmt"
 
 func main(){
 
-	// make
-
-	// x := make([]int,5)
-
-	// x = append(x, 8) // [0 0 0 0 0 0 0 0 10]
-	// fmt.Println(x)
-	// fmt.Println(cap(x))
-
-	x := []int{1,2,3,4}
-	fmt.Println(cap(x))
-	y := x[:2]
-	y = append(y,10)
-	// x = append(x,20)
-	y = append(y,10,10)
-	y = append(y,10)
-	
-	var capa = [5]int{}
+	var x string = "hello world!"
+	var b byte = x[6]
+	var y = []int{1,2,5:10,3,4,18:100}
 
 	fmt.Println("===== x =====")
-	fmt.Println(x)
-	fmt.Println(len(x))
-	fmt.Println(cap(x))
+	fmt.Println(string(b))
 	fmt.Println("===== y =====")
 	fmt.Println(y)
-	fmt.Println(len(y))
-	fmt.Println(cap(y))
 	fmt.Println("===== make =====")
-	copy(capa[0:], x)
-	fmt.Println((capa))
+	
+	var l rune = 'あ'
+	fmt.Println(l)
+	var n int = 65
+	var nn = string(n) // int to string is not hobidden but not recommended
+	fmt.Println(nn)
+
 	
 }
