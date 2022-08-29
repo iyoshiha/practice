7c55d90330f9afcec4e957d761bc3acdd8e889cd
iyoshiha <yoshi3co@gmail.com>
2022-08-29 10:58:47 +0900

    comparing and converting structs

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -12,11 +12,40 @@ func main(){
 	}
 
 	var fred person
-	bob := person{}
+	bob := person{"julia", 40, "cat"}
 
 	fmt.Println("===== person =====")
 	fmt.Println(fred)
 	fmt.Println("===== totalWins =====")
 	fmt.Println(bob)
 	fmt.Println("===== make =====")
+
+	beth := person{
+		age: 30,
+		name: "beth",
+	}
+
+	fmt.Println(beth)
+	
+	type firstPerson struct {
+		name string
+		age int
+	}
+
+	type secondPerson struct {
+		name string
+		age int
+	}
+
+	var g struct {
+		name 	string
+		age		int
+	}
+
+	f := firstPerson{"bob", 50}
+	g = f
+	fmt.Println(g)
+
+
+
 }
