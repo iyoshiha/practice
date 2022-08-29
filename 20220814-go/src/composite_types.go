1956cb419f290c133319bc85dab5c2ec04409265
iyoshiha <yoshi3co@gmail.com>
2022-08-29 18:42:55 +0900

    english

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -2,29 +2,12 @@ package main
 
 import "fmt"
 
-type MyfuncOpts struct {
-	FirstName string
-	LastName string
-	Age int
-}
 
 func main(){
 
-	fmt.Println(div(100, 0))
-	fmt.Println(MyfuncOpts{FirstName:"ba",Age:10})
+	fmt.Println()
+	fmt.Println()
 
 
 
-}
-
-func myfunc(opts MyfuncOpts) int{
-
-	return opts.Age 
-}
-
-func div(numerator int, denominator int) int {
-	if denominator == 0{
-		return 0
-	}
-	return numerator / denominator
 }
