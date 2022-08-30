486e6a1168310c070654fc4e3203e187ae8983a6
iyoshiha <yoshi3co@gmail.com>
2022-08-30 09:01:16 +0900

    asd

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -1,15 +1,51 @@
 package main
 
 import (
-	. "fmt"
-	"math/rand"
+	"fmt"
 )
 
 
 func main(){
 	
-	// blocks 
-	n := rand.Intn(10)
-	
-	Println(n)
+	// nested code sucks  
+
+// 	for i := 1; i <= 100; i++ {
+// if i%3 == 0 {
+// if i%5 == 0 {
+// fmt.Println("FizzBuzz")
+// } else {
+// fmt.Println("Fizz")
+// }
+// } else if i%5 == 0 {
+// fmt.Println("Buzz")
+// } else {
+// fmt.Println(i)
+// }
+// }
+
+
+	// short is encourages in go 
+
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+			continue
+		}
+		if i%3 == 0 {
+			fmt.Println("Fizz")
+			continue
+		}
+		if i%5 == 0 {
+			fmt.Println("Buzz")
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	// for - range 
+
+	evenVals := []int{2,4,6,8,10,12}
+	for i, v := range evenVals {
+		fmt.Println(i, v)
+	}
 }
