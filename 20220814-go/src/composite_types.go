78beb245658637c940ed8ba4e61ab999a3c26e8f
iyoshiha <yoshi3co@gmail.com>
2022-08-31 23:11:02 +0900

    t

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -7,73 +7,16 @@ import (
 
 func main(){
 	
-	// nested code sucks  
+	samples := []string{"hello", "apple_n!"}
+	outer:
+		for _, sample := range samples {
+			for i, r := range sample {
+				fmt.Println(i, r, string(r))
+				if r == 'l' {
+					continue outer
+				}
+			}
+			fmt.Println()
 
-// 	for i := 1; i <= 100; i++ {
-// if i%3 == 0 {
-// if i%5 == 0 {
-// fmt.Println("FizzBuzz")
-// } else {
-// fmt.Println("Fizz")
-// }
-// } else if i%5 == 0 {
-// fmt.Println("Buzz")
-// } else {
-// fmt.Println(i)
-// }
-// }
-
-
-	// short is encourages in go 
-
-	for i := 1; i <= 100; i++ {
-		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("FizzBuzz")
-			continue
 		}
-		if i%3 == 0 {
-			fmt.Println("Fizz")
-			continue
-		}
-		if i%5 == 0 {
-			fmt.Println("Buzz")
-			continue
-		}
-		fmt.Println(i)
-	}
-
-	// for - range 
-
-	evenVals := []int{2,4,6,8,10,12}
-	for _, v:= range evenVals {
-		fmt.Println(v)
-	}
-
-	m := map[string]int{
-		"a": 1,
-		"b": 2,
-		"c": 3,
-	}
-
-	for i := 0; i < 3; i++ {
-		fmt.Println("Loop", i)
-		for k,v := range m {
-			fmt.Println(k,v)
-		}
-	}
-
-	fmt.Println("===========")
-
-	// number 7 will be skip with π 
-	// because iterating over the runes 
-	samples := []string{"hello", "apple_π!"}
-	for _, sample := range samples {
-		for i, r := range sample {
-			fmt.Println(i, r, string(r))
-		}
-		fmt.Println()
-	}
-
-
-
 }
