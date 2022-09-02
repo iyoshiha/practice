248036d6f454379c86702ec9afb56edbcd3c4fbf
iyoshiha <yoshi3co@gmail.com>
2022-09-02 23:41:14 +0900

    missing label switch

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -21,5 +21,26 @@ func main(){
 		}
 	}
 
+	missingLabelSwitch()
+
+
+}
+
+
+func missingLabelSwitch () {
+
+	for i := 0; i < 10; i++ {
+		switch {
+		case i%2 == 0:
+			fmt.Println(i, "is even")
+		case i%3 == 0:
+			fmt.Println(i, "is divisible by 3 but not 2")
+		case i%7 == 0:
+			fmt.Println("exit the loop")
+			break
+		default:
+			fmt.Println(i, "is boring")
+		}
+	}
 
 }
