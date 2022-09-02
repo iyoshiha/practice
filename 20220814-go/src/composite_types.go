c16ff03c394b4aa05da24f70f8b0e773b1b29901
iyoshiha <yoshi3co@gmail.com>
2022-09-02 22:59:58 +0900

    review lavel outer

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -9,9 +9,9 @@ func main(){
 	
 	samples := []string{"hello", "apple_n!"}
 	outer:
-		for _, sample := range samples {
+		for ii, sample := range samples {
 			for i, r := range sample {
-				fmt.Println(i, r, string(r))
+				fmt.Println(ii, i, r, string(r))
 				if r == 'l' {
 					continue outer
 				}
