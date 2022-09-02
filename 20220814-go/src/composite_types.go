53132e454b4ce4a28316ba4c50a521762a591dba
iyoshiha <yoshi3co@gmail.com>
2022-09-02 23:47:12 +0900

    moved expressions into function standardSwitch

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -7,6 +7,14 @@ import (
 
 func main(){
 	
+ 	StandardSwitch()
+	missingLabelSwitch()
+
+
+}
+
+func StandardSwitch() {
+
 	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologlist"}
 	for _, word := range words {
 		switch size := len(word); size {
@@ -20,27 +28,24 @@ func main(){
 			fmt.Println(word, "is a long word!")
 		}
 	}
-
-	missingLabelSwitch()
-
-
 }
 
 
 func missingLabelSwitch () {
 
-	for i := 0; i < 10; i++ {
-		switch {
-		case i%2 == 0:
-			fmt.Println(i, "is even")
-		case i%3 == 0:
-			fmt.Println(i, "is divisible by 3 but not 2")
-		case i%7 == 0:
-			fmt.Println("exit the loop")
-			break
-		default:
-			fmt.Println(i, "is boring")
+	loop:
+		for i := 0; i < 10; i++ {
+			switch {
+			case i%2 == 0:
+				fmt.Println(i, "is even")
+			case i%3 == 0:
+				fmt.Println(i, "is divisible by 3 but not 2")
+			case i%7 == 0:
+				fmt.Println("exit the loop")
+				break loop
+			default:
+				fmt.Println(i, "is boring")
+			}
 		}
-	}
 
 }
