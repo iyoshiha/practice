da48a3717e1ce71fa184ab132707da0f42fa55d8
iyoshiha <yoshi3co@gmail.com>
2022-09-02 23:33:27 +0900

    switch

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -7,16 +7,19 @@ import (
 
 func main(){
 	
-	samples := []string{"hello", "apple_n!"}
-	outer:
-		for ii, sample := range samples {
-			for i, r := range sample {
-				fmt.Println(ii, i, r, string(r))
-				if r == 'l' {
-					continue outer
-				}
-			}
-			fmt.Println()
-
+	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologlist"}
+	for _, word := range words {
+		switch size := len(word); size {
+		case 1,2,3,4:
+			fmt.Println(word, "is a short word!")
+		case 5:
+			wordLen := len(word)
+			fmt.Println(word, "is exactly theright length:", wordLen)
+		case 6,7,8,9:
+		default:
+			fmt.Println(word, "is a long word!")
 		}
+	}
+
+
 }
