ab7c1e4b40bd5761a233b49a7d3f3d6dc3dbaf50
iyoshiha <yoshi3co@gmail.com>
2022-09-03 16:33:03 +0900

    supply slice for variadic

--- 20220814-go/src/function_chapter_5.go
+++ 20220814-go/src/function_chapter_5.go
@@ -5,9 +5,25 @@ import (
 )
 
 func main() {
+	fmt.Println(addTo(3))
+	fmt.Println(addTo(3, 2))
+	fmt.Println(addTo(3, 2,4,5,6,8))
+	a := []int{4,3}
+	fmt.Println(addTo(3,a...))
+	fmt.Println(addTo(3,[]int{1,2,3,4,5}...))
 
 }
 
+func addTo(base int, vals ...int) []int {
+	out := make([]int, 0, len(vals))
+	for _, v := range vals {
+		out = append(out,  base+v)
+	}
+	return out
+}
+
+func variadicInputParameters() {
+}
 
 // you can write your input when multiple input prameters of the same type
 func div(numerator, denominator int) int {
