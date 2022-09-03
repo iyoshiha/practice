6f94b8edee0415c78cae48b34d863a21c5b114c7
iyoshiha <yoshi3co@gmail.com>
2022-09-03 09:11:52 +0900

    valid goto

--- 20220814-go/src/composite_types.go
+++ 20220814-go/src/composite_types.go
@@ -5,13 +5,22 @@ import (
 	"math/rand"
 )
 
-
 func main(){
 	validGoto()
 }
 
 func validGoto() {
-
+	a := rand.Intn(10)
+	for a < 100 {
+		if a%5 == 0 {
+			goto done
+		}
+		a = a*2 + 1
+	}
+	fmt.Println("do sth when the loop completes normally")
+done:
+	fmt.Println("do complicated stuff no matter why we left the loop")
+	fmt.Println(a)
 }
 
 /* 
