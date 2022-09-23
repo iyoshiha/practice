8ffb27f79c202e5647dd2e47cee407518abb8130
iyoshiha <yoshi3co@gmail.com>
2022-09-23 23:37:03 +0900

    main.go : newPractice3

--- 20220814-go/src/pointer_6/main.go
+++ 20220814-go/src/pointer_6/main.go
@@ -12,8 +12,13 @@ func main() {
 	// false
 	x = new(int)
 	Println(x == nil)
+	Println(x)
+	Println(*x)
 
 	// false
+	// you cant use an & before a primitive time 
+	// when you need a primitive type pointer
+	// declare a variable and point to it
 	var y int
 	x = &y
 	Println(x == nil)
